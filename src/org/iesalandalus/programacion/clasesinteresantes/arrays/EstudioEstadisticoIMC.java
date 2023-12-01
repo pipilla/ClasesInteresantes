97bfcd737add6b96b4cd5342053f4a5c94189a16
Juan Miguel <juanmi-64@hotmail.com>
2023-12-01 13:45:03 +0100

    Añado las clases de los ejercicios.

--- src/org/iesalandalus/programacion/clasesinteresantes/arrays/EstudioEstadisticoIMC.java
+++ src/org/iesalandalus/programacion/clasesinteresantes/arrays/EstudioEstadisticoIMC.java
@@ -1,5 +1,38 @@
 package org.iesalandalus.programacion.clasesinteresantes.arrays;
 
-public class EstudioEstadisticoIMC {
+import org.iesalandalus.programacion.utilidades.Entrada;
 
+public class EstudioEstadisticoIMC {
+    public static void main(String[] args) {
+        String nombre;
+        int peso;
+        int altura;
+        int contadorImc = 1;
+        char respuesta = 'n';
+        do {
+            System.out.print("Dime el nombre del sujeto nª" + contadorImc + ": ");
+            nombre = Entrada.cadena();
+            do {
+                System.out.print("Dime el peso en kg del primer sujeto: ");
+                peso = Entrada.entero();
+            } while (peso <= 0);
+            do {
+                System.out.print("Dime la altura en cm del sujeto: ");
+                altura = Entrada.entero();
+            } while (altura <= 0);
+            double alturaMetros = (double) altura / 100;
+            double imc = peso / (alturaMetros * alturaMetros);
+            System.out.println(imc);
+            contadorImc++;
+            if (contadorImc > 2) {
+                do {
+                    System.out.print("¿Quiéres seguir añadiendo sujetos? s/n:");
+                    respuesta = Entrada.caracter();
+                } while (respuesta == 's' || respuesta == 'n');
+            }
+            double[] conjuntoImc = new double[contadorImc];
+            conjuntoImc[contadorImc] = imc;
+        } while (respuesta == 'n');
+
+    }
 }
