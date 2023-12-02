456fdeaf4bd754ea42409add945552255f9af40c
Juanmi <juanmi-64@hotmail.com>
2023-12-02 10:15:22 +0100

    Subo los ejercicios.

--- src/org/iesalandalus/programacion/clasesinteresantes/arrays/EstudioEstadisticoIMC.java
+++ src/org/iesalandalus/programacion/clasesinteresantes/arrays/EstudioEstadisticoIMC.java
@@ -8,7 +8,16 @@ public class EstudioEstadisticoIMC {
         int peso;
         int altura;
         int contadorImc = 1;
-        char respuesta = 'n';
+        char respuesta = 's';
+        double media = 0;
+        double mayorImc = 0;
+        int posicionMayorImc = 0;
+        double menorImc = 0;
+        int posicionMenorImc = 0;
+        int sujetosEncimaMedia = 0;
+        int sujetosDebajoMedia = 0;
+        String[] conjuntoNombres = new String[100];
+        double[] conjuntoImc = new double[100];
         do {
             System.out.print("Dime el nombre del sujeto nª" + contadorImc + ": ");
             nombre = Entrada.cadena();
@@ -23,16 +32,39 @@ public class EstudioEstadisticoIMC {
             double alturaMetros = (double) altura / 100;
             double imc = peso / (alturaMetros * alturaMetros);
             System.out.println(imc);
+            if (contadorImc == 1) {
+                mayorImc = imc;
+                menorImc = imc;
+            }
+            if (imc > mayorImc) {
+                posicionMayorImc = contadorImc;
+            }
+            if (imc < menorImc) {
+                posicionMenorImc = contadorImc;
+            }
+            conjuntoNombres[contadorImc] = nombre;
+            conjuntoImc[contadorImc] = imc;
+            media += imc;
             contadorImc++;
             if (contadorImc > 2) {
                 do {
                     System.out.print("¿Quiéres seguir añadiendo sujetos? s/n:");
                     respuesta = Entrada.caracter();
-                } while (respuesta == 's' || respuesta == 'n');
+                } while (respuesta != 's' && respuesta != 'n');
             }
-            double[] conjuntoImc = new double[contadorImc];
-            conjuntoImc[contadorImc] = imc;
-        } while (respuesta == 'n');
-
+        } while (respuesta == 's');
+        media /= contadorImc - 1;
+        for (int i = 0; i < contadorImc; i++) {
+            if (conjuntoImc[i] > media) {
+                sujetosEncimaMedia++;
+            } else if (conjuntoImc[i] < media){
+                sujetosDebajoMedia++;
+            }
+        }
+        System.out.println("La media del IMC de dichos sujetos es de: " + media);
+        System.out.println("El sujeto con mayor IMC es: " + conjuntoNombres[posicionMayorImc] + ". Con el IMC: " + conjuntoImc[posicionMayorImc]);
+        System.out.println("El sujeto con menor IMC es: " + conjuntoNombres[posicionMenorImc] + ". Con el IMC: " + conjuntoImc[posicionMenorImc]);
+        System.out.println("Hay " + sujetosEncimaMedia + " sujetos con un IMC por encima de la media");
+        System.out.println("Hay " + sujetosDebajoMedia + " sujetos con un IMC por debajo de la media");
     }
 }
